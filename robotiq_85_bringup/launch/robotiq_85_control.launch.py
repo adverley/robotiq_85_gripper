8c3fe8930e03ab24356ff2db8f17c831e0b1c56f
tlips <thomas.lips@ugent.be>
2022-01-24 13:38:24 +0000

    add visual links to gripper tip

--- robotiq_85_bringup/launch/robotiq_85_control.launch.py
+++ robotiq_85_bringup/launch/robotiq_85_control.launch.py
@@ -1,8 +1,9 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.conditions import IfCondition, UnlessCondition
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
@@ -14,8 +15,37 @@ def generate_launch_description():
             "use_fake_controller", default_value="false", description="Use fake controller to simulate gripper?"
         )
     )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "prefix",
+            default_value="",
+            description="Prefix of the joint_names, if changed also change the controller configuration.",
+        )
+    )
+
+    prefix = LaunchConfiguration("prefix")
     use_fake_controller = LaunchConfiguration("use_fake_controller")
 
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare("robotiq_85_description"), "urdf", "robotiq_85_gripper.urdf.xacro"]
+            ),
+            " ",
+            "prefix:=",
+            prefix,
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+
+    robot_state_pub_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="screen",
+        parameters=[robot_description],
+    )
     gripper_driver_node = Node(
         package="robotiq_85_driver",
         executable="robotiq_85_driver",
@@ -25,8 +55,13 @@ def generate_launch_description():
         output="screen",
     )
 
-    # TODO: start up the Controller
-
+    gripper_controller_node = Node(
+        package="robotiq_85_driver",
+        executable="single_robotiq_85_action_server",
+        name="robotiq_85_controller",
+        condition=UnlessCondition(use_fake_controller),
+        output="screen",
+    )
     fake_gripper_controller_node = Node(
         package="robotiq_85_driver",
         executable="robotiq_85_fake_action_server",
@@ -35,4 +70,6 @@ def generate_launch_description():
         output="screen",
     )
 
-    return LaunchDescription(declared_arguments + [fake_gripper_controller_node, gripper_driver_node])
+    nodes_to_start = [robot_state_pub_node, fake_gripper_controller_node, gripper_controller_node, gripper_driver_node]
+
+    return LaunchDescription(declared_arguments + nodes_to_start)
